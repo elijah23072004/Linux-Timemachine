d0975a23257862579f02e7a569f53e2c13afef3b
Eli <elijah2307.williams@gmail.com>
2025-04-22 16:06:35 +0100

    added install.sh which compiles and moves executables to /usr/local/bin

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+build_dir="./build_dir"
+path_loc="/usr/local/bin/"
+
+meson setup $build_dir 
+meson compile -C $build_dir 
+
+
+if test -w $path_loc; then 
+    cp ${build_dir}/linuxTimeMachineBackend ${path_loc}linuxTimeMachineCLI
+    cp ${build_dir}/linuxTimeMachineApp ${path_loc}linuxTimeMachine
+else
+    echo "Current process does not have permission to write to location $path_loc please run with sufficient permissions (e.g via sudo)"
+fi 
+
+
+
+
+rm -r $build_dir 
+
